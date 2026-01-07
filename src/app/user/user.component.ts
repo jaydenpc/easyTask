4b8f4e394fc6701ab6715dccf3def0f8f8f57e9f
Jayden Cruz <jaydencruz1121@gmail.com>
2026-01-06 19:18:17 -1000

    Remove random aspect to user component & Create list of users

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,4 @@
-import { Component , computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users'; // import dummy users array from selected folder
-
-// get a random index between 0 and the length of the DUMMY_USERS array
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component , Input} from '@angular/core';
 
 @Component({
   
@@ -14,23 +10,13 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 })
 
 
-
-// properties of Component class are usuable inside the template
 export class UserComponent {
-  public selectedUser = signal(DUMMY_USERS[randomIndex]); // select random user from the dummy users array
+  @Input() avatar!: string;
+  @Input() name!: string;
 
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
-  
-
-  //get imagePath() {
-  //  return 'assets/users/' + this.selectedUser.avatar;
-  //}
-
-  onUserSelect() {
-    //console.log('clicked');
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
-    //this.selectedUser = DUMMY_USERS[randomIndex];
-
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
   }
+
+  onUserSelect() {}
 }
