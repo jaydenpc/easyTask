124a0e7366fc9fbe1b359fd7901dbba30d860eb6
Jayden Cruz <jaydencruz1121@gmail.com>
2026-01-08 17:57:15 -1000

    Outsource task and user interface into model files

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,6 @@
 import { Component , EventEmitter, Input, Output} from '@angular/core';
 
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-};
-
-
+import { type User } from './user.model';
 
 @Component({
   
