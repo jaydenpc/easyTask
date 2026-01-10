f0851107a718ee5b0dff95056187dff3050fc03d
Jayden Cruz <jaydencruz1121@gmail.com>
2026-01-09 15:50:47 -1000

    Add complete event for tasks

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -40,6 +40,10 @@ export class TasksComponent {
     return this.tasks.filter((task) => task.userId === this.userId);
   }
 
+  onCompleteTask(id: string) {
+    this.tasks = this.tasks.filter(task => task.id !== id);
+  }
+
 
 
   
